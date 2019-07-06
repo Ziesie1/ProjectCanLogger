9a20ef0a5649d6aa9deae761f2791b6225288a69
Ziesie <47595958+Ziesie1@users.noreply.github.com>
2019-07-06 17:09:28 +0200

    Displayansteuerung (langsam)
    Die Ansteuerung erfolgt über Software Serial, daher ist dieses sehr langsam.
    Alternativ kann über Hardware Serial angesteuert werden, dafür muss das Display allerdings anders angeschlossen werden.

--- CanLogger/src/main.cpp
+++ CanLogger/src/main.cpp
@@ -1,12 +1,26 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+
+
+Adafruit_ILI9341 display = Adafruit_ILI9341{PC9, PA8, PA10, PB5, PC8}; // use Software Serial
+//Adafruit_ILI9341 display = Adafruit_ILI9341{PC9, PA8, PC8}; // use Hardware Serial
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  display.begin();
+  display.fillScreen(ILI9341_BLACK);
+
+  display.setTextSize(3);
+  display.print("Test eines Textes...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello World!");
-  delay(1000);
+  delay(3000);
 }
+
