8e052a3c3cfd06dafd747d659ecc29d17f02580c
Falk97 <47714859+Falk97@users.noreply.github.com>
2019-07-08 20:11:56 +0200

    Encoder Test
    Einfaches Programm um den Encoder zu verstehen. Die Interruptroutine muss noch angepasst werden.

--- CanLogger/src/main.cpp
+++ CanLogger/src/main.cpp
@@ -12,6 +12,45 @@ SdFat sdKarte{};
 #define errorExit(msg) errorHalt(F(msg))
 #define initError(msg) initErrorHalt(F(msg))
 
+//Variablen zur Auswertung des Drehencoders
+int oldval = 1;
+constexpr int encoderPinA = PC7;
+constexpr int encoderPinB = PC6;
+volatile int encoderPos = 0;
+int encoder0PinALast = LOW;
+boolean rotating = false;
+boolean A_set = false;            
+boolean B_set = false;
+
+void doEncoderA(){
+
+  if ( rotating ) {
+    delay (1);  // wait a little until the bouncing is done
+    if( digitalRead(encoderPinA) != A_set ) {  // debounce once more
+      A_set = !A_set;
+      if ( A_set && !B_set ){ 
+        encoderPos ++;
+      rotating = false; 
+      } // no more debouncing until loop() hits again
+    }
+  }
+}
+void doEncoderB(){
+
+  if ( rotating ){
+    delay (1);
+    if( digitalRead(encoderPinB) != B_set ) {
+      B_set = !B_set;
+      //  adjust counter - 1 if B leads A
+      if( B_set && !A_set ){ 
+        encoderPos --;
+        rotating = false;
+      }
+    }
+  }
+}
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -35,12 +74,28 @@ void setup() {
     }
   }
 
+  pinMode (encoderPinA, INPUT);
+  pinMode (encoderPinB, INPUT);
 
+  //digitalWrite(encoderPinA,HIGH);
+  //digitalWrite(encoderPinB,HIGH);
+
+  attachInterrupt(0,doEncoderA,CHANGE);
+  attachInterrupt(1,doEncoderB,CHANGE);
 }
 
 void loop() {
-
+  
+  rotating = true;
+  if(oldval != encoderPos)
+  {
+    Serial.println(encoderPos);
+    oldval = encoderPos;
+  }
+  
 }
+
+
 
 void serialEvent() {
   while (Serial.available()) {
@@ -53,4 +108,8 @@ void serialEvent() {
     Canmsg msg{};
     Serial.println(msg);
   }
+  
 }
+
+
+
