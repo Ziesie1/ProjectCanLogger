3e4bd889b4e33bc5df07764d739826157e2bea84
Falk97 <47714859+Falk97@users.noreply.github.com>
2019-07-09 19:21:53 +0200

    Update Encoder

--- CanLogger/src/main.cpp
+++ CanLogger/src/main.cpp
@@ -4,49 +4,16 @@
 #include "sd/SD.hpp"
 #include "Canmsg.h"
 #include "serial/SerialCommunication.hpp"
+#include "buttons/Encoder.hpp"
 
 Adafruit_ILI9341 display = Adafruit_ILI9341{PC9, PA8, PA10, PB5, PC8}; // use Software Serial
 //Adafruit_ILI9341 display = Adafruit_ILI9341{PC9, PA8, PC8}; // use Hardware Serial
 
 using namespace utilities; // für scom
 
-//Variablen zur Auswertung des Drehencoders
-int oldval = 1;
-constexpr int encoderPinA = PC7;
-constexpr int encoderPinB = PC6;
-volatile int encoderPos = 0;
-int encoder0PinALast = LOW;
-boolean rotating = false;
-boolean A_set = false;            
-boolean B_set = false;
 
-void doEncoderA(){
 
-  if ( rotating ) {
-    delay (1);  // wait a little until the bouncing is done
-    if( digitalRead(encoderPinA) != A_set ) {  // debounce once more
-      A_set = !A_set;
-      if ( A_set && !B_set ){ 
-        encoderPos ++;
-      rotating = false; 
-      } // no more debouncing until loop() hits again
-    }
-  }
-}
-void doEncoderB(){
 
-  if ( rotating ){
-    delay (1);
-    if( digitalRead(encoderPinB) != B_set ) {
-      B_set = !B_set;
-      //  adjust counter - 1 if B leads A
-      if( B_set && !A_set ){ 
-        encoderPos --;
-        rotating = false;
-      }
-    }
-  }
-}
 
 void setup() {
   Serial.begin(115200);
@@ -60,26 +27,14 @@ void setup() {
   display.setTextSize(3);
   display.print("Test eines Textes...");
 
-  pinMode (encoderPinA, INPUT);
-  pinMode (encoderPinB, INPUT);
-
-  //digitalWrite(encoderPinA,HIGH);
-  //digitalWrite(encoderPinB,HIGH);
-
-  //attachInterrupt(0,doEncoderA,CHANGE);
-  //attachInterrupt(1,doEncoderB,CHANGE);
+  
 
   scom << "CanLogger ist Initialisiert" << endz;
 }
 
 void loop() {
   
-  rotating = true;
-  if(oldval != encoderPos)
-  {
-    Serial.println(encoderPos);
-    oldval = encoderPos;
-  }
+  
   
 }
 
