facdc2ceb62e6eee3c865b3d71947e840df4fc6c
Falk97 <47714859+Falk97@users.noreply.github.com>
2019-08-29 18:51:25 +0200

    Umsetzung Coderichtlinien
    Für die Klasse Textzeile

--- CanLogger/src/display/elements/Textzeile.cpp
+++ CanLogger/src/display/elements/Textzeile.cpp
@@ -1,28 +1,48 @@
 #include "display/elements/Textzeile.hpp"
 #include "utilities/utilities.hpp"
 
+/*
+    Constructor of the class Textzeile
+    Input: display - Reference of the display where the "Textzeile" will be printed on,
+           isSelected - selection status, referring to the functionality of scrollingthis attribut becomes important
+           offsetXSpalte1 - X offset of the tables first vertical gridline
+           offsetXSpalte2 - X offset of the tables second vertical gridline
+           offsetHeadline1 - X offset of the Headline "Idf."  
+           offsetHeadline2 - X offset of the Headline "Time" 
+           offsetHeadline3 - X offset of the Headline "Data"
+           zeilenhoehe - height of each "Textzeile" when printed 
+    Return: -
+*/
 Textzeile::Textzeile(ILI9341& display, bool isSelected, uint8_t offsetXSpalte1,
-                     uint8_t offsetXSpalte2, uint8_t offsetHeadline1,uint8_t offsetHeadline2,uint8_t offsetHeadline3,uint8_t zeilenhoehe)
+                     uint8_t offsetXSpalte2, uint8_t offsetHeadline1, uint8_t offsetHeadline2, 
+                     uint8_t offsetHeadline3, uint8_t zeilenhoehe)
         :display{display},isSelected{isSelected},offsetXSpalte1{offsetXSpalte1},offsetXSpalte2{offsetXSpalte2},offsetXHeadline1{offsetHeadline1},offsetXHeadline2{offsetHeadline2},offsetXHeadline3{offsetHeadline3},zeilenhoehe{zeilenhoehe}
 {
-    //screenBuffer_getMessageAtPosition(this->message,this->pos);
+    
 }
 
-void Textzeile::printImportantContent(uint8_t posY,unsigned long Farbe)
+/*
+    This methode prints the most important content of the CAN-messages of the frontend buffer into the logtable shown on the display
+    Input: posY - Y coordinate of the printing start 
+           farbe - printing colour 
+           
+    Return: -
+*/
+void Textzeile::printImportantContent(uint8_t posY, unsigned long farbe)
 {
     
     //erste Spalte mit Begrenzung
     String s1 = String(this->message.GetFullId(),HEX);
     AddBlanksToString(s1,8);
     s1.toUpperCase();
-    this->display.printString(this->offsetXHeadline1,posY,s1.c_str(),Farbe,this->COLOR_BACKGROUND_UNSELECTED);
+    this->display.printString(this->offsetXHeadline1,posY,s1.c_str(),farbe,this->COLOR_BACKGROUND_UNSELECTED);
     this->display.drawVerticalLine(this->offsetXSpalte1,posY,posY+this->zeilenhoehe,this->COLOR_TABLE_LINE);
 
     //zweite Spalte mit Begrenzung
     s1 = String(this->message.GetTime(),HEX);
     AddBlanksToString(s1,4);
     s1.toUpperCase();
-    this->display.printString(this->offsetXHeadline2,posY,s1.c_str(),Farbe,this->COLOR_BACKGROUND_UNSELECTED);
+    this->display.printString(this->offsetXHeadline2,posY,s1.c_str(),farbe,this->COLOR_BACKGROUND_UNSELECTED);
     this->display.drawVerticalLine(this->offsetXSpalte2,posY,posY+this->zeilenhoehe,this->COLOR_TABLE_LINE);
 
     //dritte Spalte 
@@ -42,27 +62,47 @@ void Textzeile::printImportantContent(uint8_t posY,unsigned long Farbe)
         s1 += String(this->message.GetCanLength());
     }
     AddBlanksToString(s1,16);
-    this->display.printString(this->offsetXSpalte2+4,posY,s1.c_str(),Farbe,this->COLOR_BACKGROUND_UNSELECTED);
+    this->display.printString(this->offsetXSpalte2+4,posY,s1.c_str(),farbe,this->COLOR_BACKGROUND_UNSELECTED);
         
     
 }
-void Textzeile::printWholeContent()
-{
-    //hier wird detailview geöffnet
-}
+
+/*
+    This methode changes the selection status to true
+    input: -
+    return: -
+*/
 void Textzeile::selectZeile()
 {
     this->isSelected = true;
 }
+
+/*
+    This methode changes the selection status to false
+    input: -
+    return: -
+*/
 void Textzeile::unselectZeile()
 {
     this->isSelected = false;
 }
+
+/*
+    This methode returns the RTR status of the illustrazed CAN-message
+    input: -
+    return: true - CAN-message is a RTR-message
+            false - CAN-message isn't a RTR-message
+*/
 bool Textzeile::isRtr()
 {
     return this->message.GetRtr();
 }
 
+/*
+    This methode sets CAN-message of the "Textzeile"
+    input: msg - reference to a CAN-message of the frontend buffer
+    return: -
+*/
 void Textzeile::setCanMsg(Canmsg& msg)
 {
     this->message = msg;
