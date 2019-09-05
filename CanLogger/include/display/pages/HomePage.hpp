ba002695c0658e2c428023c0d4182f7c4166ff6b
Falk97 <47714859+Falk97@users.noreply.github.com>
2019-09-05 18:45:56 +0200

    Button EInstellungen
    Die HomePage besitzt jetzt drei Button

--- CanLogger/include/display/pages/HomePage.hpp
+++ CanLogger/include/display/pages/HomePage.hpp
@@ -5,6 +5,7 @@
 #include "display/pages/DisplayPage.hpp"
 #include "display/elements/Button.hpp"
 #include "display/elements/Arrow.hpp"
+#include "display/elements/Table.hpp"
 /*
     This Class creates the homescreen page and prints it on the display.
 */
@@ -16,13 +17,13 @@ protected:
     const uint16_t BUTTON_HIGH = 32;
     const uint16_t BUTTON_MID_DIST = 20;
     const uint16_t BUTTON_Y_AXE_OFFSET = 80;
+    const uint16_t FIRST_BUTTON_Y_OFFSET = 160;
+    const uint16_t BUTTON_Y_DISTANCE = 20;
     const byte BUTTON_TEXT_DST_X = 5;
     const byte BUTTON_TEXT_DST_Y = 8;
-    const byte COLOR_BUTTON_DEFAULT[3] = {1, 64, 118};
-    unsigned long colorButtonDefault;
+    const unsigned long COLOR_BUTTON_DEFAULT = Table::COLOR_BLUE_BACKGROUND_HEADER;
     const unsigned long COLOR_BUTTON_TEXT = WHITE;
-    const byte COLOR_ARROW_SELECTED[3] = {237, 125, 49};
-    unsigned long colorArrowSelected;
+    unsigned long COLOR_ARROW_SELECTED = Table::COLOR_ORANGE_WRITING_BODY_ISRTR;
     const unsigned long COLOR_ARROW_UNSELECTED = WHITE;
     const uint16_t IMAGE_DST_Y = 15;
 
@@ -35,6 +36,8 @@ protected:
     Arrow*  pfeilNichtSpeichern;
     Button* buttonSpeichern;
     Arrow* pfeilSpeichern;
+    Button* buttonEinstellungen;
+    Arrow* pfeilEinstellungen;
    
 public:
     HomePage(ILI9341& display);
--- CanLogger/src/display/pages/HomePage.cpp
+++ CanLogger/src/display/pages/HomePage.cpp
@@ -16,9 +16,9 @@ extern DisplayPageManager pageManager; // verweißt auf das Objekt in Main
 HomePage::HomePage(ILI9341& display)
     :display{display}, statusSD{false}
 {
-    this->colorButtonDefault = this->display.makeColor(this->COLOR_BUTTON_DEFAULT[0], this->COLOR_BUTTON_DEFAULT[1], this->COLOR_BUTTON_DEFAULT[2]);
-    this->colorArrowSelected = this->display.makeColor(this->COLOR_ARROW_SELECTED[0], this->COLOR_ARROW_SELECTED[1], this->COLOR_ARROW_SELECTED[2]);
+    
 }
+
 /*
     The destructor of the class HomePage calls the destructors of all created objects.
 */
@@ -60,9 +60,11 @@ void HomePage::loop()
         {
             this->statusSD = false;
             this->buttonNichtSpeichern->selectButton();
-            this->pfeilNichtSpeichern->draw(this->colorArrowSelected);
+            this->pfeilNichtSpeichern->draw(this->COLOR_ARROW_SELECTED);
             this->buttonSpeichern->unselectButton();
             this->pfeilSpeichern->draw(this->COLOR_ARROW_UNSELECTED);
+            this->buttonEinstellungen->unselectButton();
+            this->pfeilEinstellungen->draw(this->COLOR_ARROW_UNSELECTED);
         }
         if(getEncoderPos() == 1)
         {
@@ -70,11 +72,24 @@ void HomePage::loop()
             this->buttonNichtSpeichern->unselectButton();
             this->pfeilNichtSpeichern->draw(this->COLOR_ARROW_UNSELECTED);
             this->buttonSpeichern->selectButton();
-            this->pfeilSpeichern->draw(this->colorArrowSelected);
+            this->pfeilSpeichern->draw(this->COLOR_ARROW_SELECTED);
+            this->buttonEinstellungen->unselectButton();
+            this->pfeilEinstellungen->draw(this->COLOR_ARROW_UNSELECTED);
         }
-        if(getEncoderPos() > 1)
+        if(getEncoderPos() == 2)
         {
-            setEncoderPos(1);
+            this->statusSD = false;
+            this->buttonNichtSpeichern->unselectButton();
+            this->pfeilNichtSpeichern->draw(this->COLOR_ARROW_UNSELECTED);
+            this->buttonSpeichern->unselectButton();
+            this->pfeilSpeichern->draw(this->COLOR_ARROW_UNSELECTED);
+            this->buttonEinstellungen->selectButton();
+            this->pfeilEinstellungen->draw(this->COLOR_ARROW_SELECTED);
+        }
+
+        if(getEncoderPos() > 2)
+        {
+            setEncoderPos(2);
         }
         if(getEncoderPos() < 0)
         {
@@ -90,15 +105,18 @@ void HomePage::startView()
 {
     this->display.fillScreen(WHITE);
 
-    uint16_t ymitte = ILI9341::VERTICAL_MAX / 2;
     uint16_t xmitte = ILI9341::HORIZONTAL_MAX / 2;
 
     this->display.drawBmp(xmitte-OSTFALIA_LOGO_SIZE_X / 2, IMAGE_DST_Y + 25, OSTFALIA_LOGO_SIZE_X, OSTFALIA_LOGO_SIZE_Y, OSTFALIA_LOGO, 1);
 
-    this->buttonNichtSpeichern = new Button{this->display, static_cast<uint16_t>(xmitte - this->BUTTON_WIDTH / 2), static_cast<uint16_t>(ymitte - this->BUTTON_MID_DIST - this->BUTTON_HIGH + this->BUTTON_Y_AXE_OFFSET),this->BUTTON_WIDTH,this->BUTTON_HIGH,this->colorButtonDefault,this->BUTTON_TEXT_DST_X,this->BUTTON_TEXT_DST_Y,"Logging - ohne Speichern", this->COLOR_BUTTON_TEXT, !this->statusSD};
-    this->buttonSpeichern = new Button{this->display, static_cast<uint16_t>(xmitte - this->BUTTON_WIDTH / 2), static_cast<uint16_t>(ymitte + this->BUTTON_MID_DIST + this->BUTTON_Y_AXE_OFFSET), this->BUTTON_WIDTH, this->BUTTON_HIGH, this->colorButtonDefault, this->BUTTON_TEXT_DST_X, this->BUTTON_TEXT_DST_Y, "Logging - mit Speichern", this->COLOR_BUTTON_TEXT, this->statusSD};
-    this->pfeilNichtSpeichern = new Arrow{this->display, this->buttonNichtSpeichern, this->colorArrowSelected, this->COLOR_ARROW_UNSELECTED, !this->statusSD};
-    this->pfeilSpeichern = new Arrow{this->display, this->buttonSpeichern, this->colorArrowSelected, this->COLOR_ARROW_UNSELECTED, this->statusSD};
+    this->buttonNichtSpeichern = new Button{this->display, static_cast<uint16_t>(xmitte - this->BUTTON_WIDTH / 2), static_cast<uint16_t>(this->FIRST_BUTTON_Y_OFFSET),this->BUTTON_WIDTH,this->BUTTON_HIGH,this->COLOR_BUTTON_DEFAULT,this->BUTTON_TEXT_DST_X,this->BUTTON_TEXT_DST_Y,"Logging - ohne Speichern", this->COLOR_BUTTON_TEXT, !this->statusSD};
+    this->buttonSpeichern = new Button{this->display, static_cast<uint16_t>(xmitte - this->BUTTON_WIDTH / 2), static_cast<uint16_t>(this->FIRST_BUTTON_Y_OFFSET+this->BUTTON_Y_DISTANCE+this->BUTTON_HIGH), this->BUTTON_WIDTH, this->BUTTON_HIGH, this->COLOR_BUTTON_DEFAULT, this->BUTTON_TEXT_DST_X, this->BUTTON_TEXT_DST_Y, "Logging - mit Speichern", this->COLOR_BUTTON_TEXT, this->statusSD};
+    this->buttonEinstellungen = new Button{this->display, static_cast<uint16_t>(xmitte - this->BUTTON_WIDTH / 2), static_cast<uint16_t>(this->FIRST_BUTTON_Y_OFFSET+2*(this->BUTTON_Y_DISTANCE+this->BUTTON_HIGH)), this->BUTTON_WIDTH, this->BUTTON_HIGH, this->COLOR_BUTTON_DEFAULT, static_cast<byte>((this->BUTTON_WIDTH-13*8)/2), this->BUTTON_TEXT_DST_Y, "Einstellungen", this->COLOR_BUTTON_TEXT, this->statusSD};
+
+    this->pfeilNichtSpeichern = new Arrow{this->display, this->buttonNichtSpeichern, this->COLOR_ARROW_SELECTED, this->COLOR_ARROW_UNSELECTED, !this->statusSD};
+    this->pfeilSpeichern = new Arrow{this->display, this->buttonSpeichern, this->COLOR_ARROW_SELECTED, this->COLOR_ARROW_UNSELECTED, this->statusSD};
+    this->pfeilEinstellungen = new Arrow{this->display, this->buttonEinstellungen, this->COLOR_ARROW_SELECTED, this->COLOR_ARROW_UNSELECTED, this->statusSD};
+    
 }
 
 /*
